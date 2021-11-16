d554295fd30b06c6fd13887bcdf32b60753a2e7a
anonymous anonymous <xiaojiejackiechen@gmail.com>
2021-11-16 05:43:23 +0000

    Whitelist columns in Review for ActiveAdmin

--- app/admin/reviews.rb
+++ app/admin/reviews.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Review do
 
+ permit_params :rating, :review, :reviewer_id, :movie_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
