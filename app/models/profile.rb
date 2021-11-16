824cc2832c897a4a98195d5e7c21969e1f82d188
anonymous anonymous <xiaojiejackiechen@gmail.com>
2021-11-16 05:42:52 +0000

    Generate profiles resource with devise

---
+++ app/models/profile.rb
@@ -0,0 +1,18 @@
+class Profile < ApplicationRecord
+  # Direct associations
+
+  # Indirect associations
+
+  # Validations
+
+  # Scopes
+
+  def to_s
+    email
+  end
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
