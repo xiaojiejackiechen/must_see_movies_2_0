39fdf8857b87e7dbe4ea4c1b0d21a53a369f4263
anonymous anonymous <xiaojiejackiechen@gmail.com>
2021-11-16 05:43:18 +0000

    Whitelist columns in Character for ActiveAdmin

--- app/admin/characters.rb
+++ app/admin/characters.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Character do
 
+ permit_params :movie_id, :actor_id, :character_name
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
