a537c9e9e106d8a423729884ea203c5745326f81
anonymous anonymous <xiaojiejackiechen@gmail.com>
2021-11-16 05:42:56 +0000

    Generate characters resource with ideas:scaffold

---
+++ app/models/character.rb
@@ -0,0 +1,14 @@
+class Character < ApplicationRecord
+  # Direct associations
+
+  # Indirect associations
+
+  # Validations
+
+  # Scopes
+
+  def to_s
+    movie.to_s
+  end
+
+end
---
+++ app/views/characters/_character.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! character, :id, :movie_id, :actor_id, :character_name, :created_at, :updated_at
+json.url character_url(character, format: :json)
