72afd908f7ebe2bc20a0cdef482ce0445653c28c
anonymous anonymous <xiaojiejackiechen@gmail.com>
2021-11-16 05:44:22 +0000

    Generate graphiti resources for bookmarks

---
+++ app/resources/bookmark_resource.rb
@@ -0,0 +1,12 @@
+class BookmarkResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :user_id, :integer
+  attribute :movie_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end
