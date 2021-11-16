41a174b45cc10b46ec8b9d251ded3b228777c2de
anonymous anonymous <xiaojiejackiechen@gmail.com>
2021-11-16 05:43:06 +0000

    Direct association between directors and movies

--- spec/models/director_spec.rb
+++ spec/models/director_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Director, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:movies) }
+
     end
 
     describe "InDirect Associations" do
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Movie, type: :model do
   
     describe "Direct Associations" do
 
+    it { should belong_to(:director) }
+
     it { should have_many(:bookmarks) }
 
     it { should have_many(:reviews) }
