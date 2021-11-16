885662a683efbc9402befba31d61f387090031fd
anonymous anonymous <xiaojiejackiechen@gmail.com>
2021-11-16 05:43:25 +0000

    Whitelist columns in Bookmark for ActiveAdmin

--- app/admin/bookmarks.rb
+++ app/admin/bookmarks.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Bookmark do
 
+ permit_params :user_id, :movie_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
