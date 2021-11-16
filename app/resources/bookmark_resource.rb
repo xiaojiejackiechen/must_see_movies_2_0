434ba0be4e914afa26cc4cdd3ca9b195171712a2
anonymous anonymous <xiaojiejackiechen@gmail.com>
2021-11-16 05:44:22 +0000

    Direct association between MovieResource and BookmarkResource

--- app/resources/bookmark_resource.rb
+++ app/resources/bookmark_resource.rb
@@ -7,6 +7,8 @@ class BookmarkResource < ApplicationResource
 
   # Direct associations
 
+  belongs_to :movie
+
   belongs_to :user,
              resource: ProfileResource
 
--- app/resources/movie_resource.rb
+++ app/resources/movie_resource.rb
@@ -10,6 +10,8 @@ class MovieResource < ApplicationResource
 
   # Direct associations
 
+  has_many   :bookmarks
+
   has_many   :reviews
 
   has_many   :casts,
